cd366d6eeed13a91ca3d7541c714dbeffb879c32
Momin Imtiaz <mominimtiaz0786@gmail.com>
2025-07-02 16:13:29 +0500

    fixes

--- ckanext/showcase/logic/action/get.py
+++ ckanext/showcase/logic/action/get.py
@@ -135,7 +135,7 @@ def showcase_package_list(context, data_dict):
             id_list.append(pkg_id[0])
         q = 'id:(' + ' OR '.join(['{0}'.format(x) for x in id_list]) + ')'
         _pkg_list = tk.get_action('package_search')(
-            context,
+            {**context, 'for_view': True},
             {'q': q, 'rows': 100})
         pkg_list = _pkg_list['results']
     return pkg_list
